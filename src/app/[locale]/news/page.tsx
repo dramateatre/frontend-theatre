8cbc0ad74eaaa0e6f5a6ae6749e5cae837b42693
Lado Asambadze <lado.asambadze1@gmail.com>
2024-07-31 17:06:00 +0400

    fix deploy issue

--- src/app/[locale]/news/page.tsx
+++ src/app/[locale]/news/page.tsx
@@ -2,14 +2,19 @@ import Image from 'next/image'
 import React from 'react'
 import NewsImage from '../../../../public/imgs/New.webp'
 import test from '../../../../public/imgs/village.jpg'
-import test2 from '../../../../public/imgs/test.jpg'
+import test2 from '../../../../public/imgs/Test.jpg'
 
 export default function page() {
     return (
         <main className="flex flex-col gap-10 px-5 pb-20 pt-40 md:px-40">
             <div className="relative z-50 flex w-full flex-col overflow-hidden rounded-[16px] shadow-custom md:flex-row">
                 <div className="relative h-[200px] w-[300px]">
-                    <Image fill src={NewsImage} alt="Village" className="object-cover object-center" />
+                    <Image
+                        fill
+                        src={NewsImage}
+                        alt="Village"
+                        className="object-cover object-center"
+                    />
                 </div>
                 <div className="flex h-[150px] w-[400px] flex-col items-start justify-around bg-[#1a1c2f] px-3 py-2 md:h-auto md:px-6">
                     <h1 className="text-white">რაც გინახავს ვეღარ ნახავ</h1>
