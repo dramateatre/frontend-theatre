2c16cafc375052b6c9c17b0bd204af871fdd8fdd
Lado Asambadze <lado.asambadze1@gmail.com>
2024-09-05 17:37:48 +0400

    update i18config

--- src/libs/i18n/i18nConfig.ts
+++ src/libs/i18n/i18nConfig.ts
@@ -1,5 +1,7 @@
-const i18nConfig = {
-    locales: ['ka', 'en'],
+import { Config } from 'next-i18n-router/dist/types'
+
+const i18nConfig: Config = {
+    locales: ['en', 'ka'],
     defaultLocale: 'ka',
     localeDetector: false,
 }
