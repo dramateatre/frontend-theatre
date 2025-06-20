f9471a121abb4bbbd8346bbd5afd4eb309b80fff
Lado <lado.asambadze1@gmail.com>
2025-06-20 09:39:56 +0400

    turn of linter

--- next.config.mjs
+++ next.config.mjs
@@ -5,11 +5,16 @@ const nextConfig = {
             test: /\.svg$/,
             use: { loader: '@svgr/webpack', options: { icon: true } },
         })
-
+        
         return config
     },
     images: {
         domains: ['localhost', 'api.batumitheatre.ge'],
+    },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
     },
 }
 
